bb3a7da7e980098c1684186217a5ace83b7fe62e
hackersville <hackersville@hotmail.com>
2016-12-04 14:51:26 +0530

    Add files via upload

---
+++ ai2.py
@@ -0,0 +1,10 @@
+name = ""
+fname = ""
+nname = ""
+
+class Init:
+    name = "Pritam Sebastian Edward"
+    fname = ""
+    nname = "Edward"
+
+    print ("Hello, " + name + " or I shall say, " + nname)
